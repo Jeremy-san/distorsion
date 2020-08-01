fe0674af03b7ff55e322b910583d2e58e05fea8e
Jeremy-san <shehata.jeremy@gmail.com>
2020-08-01 02:16:08 +0200

    distorsion final work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartesComponent } from './cartes/cartes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartesComponent
   ],
   imports: [
     BrowserModule,
